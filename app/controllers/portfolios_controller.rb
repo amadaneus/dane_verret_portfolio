7cb6d0151a70d0a42a42f250ac3a210296166737
amadaneus <verrtah@gmail.com>
2018-01-18 13:01:51 -0600

    Implements custom scopes for Portfolio items.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def ruby
+    @ruby_portfolio_items = Portfolio.ruby
+  end
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
