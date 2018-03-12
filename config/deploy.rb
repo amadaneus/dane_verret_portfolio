a299c7a238cc3b8122f2510a12a6a1a9e9041981
amadaneus <verrtah@gmail.com>
2018-03-11 19:56:21 -0500

    Updates deployment config file.

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.1"
+
+set :application, "dane_verret_portfolio"
+set :repo_url, "git@github.com:amadaneus/dane_verret_portfolio.git"
+set :stages, %w(production)
+set :branch, :master
+set :deploy_to, 'home/deploy/var/www/apps/deploy/'
+set :pty, true
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :keep_releases, 5
+set :rvm_type, :user
+
+set :ssh_options, {
+    forward_agent: true,
+    auth_methods: ["publickey"],
+    keys: ["~/.ssh/id_rsa"]
+}
